89972d1925811a4f20cb444b7243cc8dd5b71399
blze0515 <hello.git.captain6@naver.com>
2022-12-06 11:24:49 +0900

    221206 2교시

---
+++ SpringBoot/_03_SpringBoot_HikariCP/src/main/java/com/ezen/springboard/configuration/DataConfiguration.java
@@ -0,0 +1,52 @@
+package com.ezen.springboard.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class DataConfiguration {
+	
+	@Bean //거의 대부분 생성자 메소드 위에 선언
+	//application.properties 파일의 어떤 내용을 읽어서 설정할지 
+	@ConfigurationProperties(prefix="spring.datasource.hikari")
+	public HikariConfig hikariConfig() {
+		return new HikariConfig();
+	}
+	
+	@Bean
+	public DataSource dataSource() throws Exception {
+		DataSource dataSource = new HikariDataSource(hikariConfig());
+		return dataSource;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
